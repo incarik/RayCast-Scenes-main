cac4cdfcd7adb6ce31b928633f23da2aa2a9d339
incarik <incarik1@gmail.com>
2024-11-22 12:07:13 +0100

    script del examen, mas arreglos

--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -1,18 +1,48 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RayCast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text cuentaAtrasTexto;
+    public int cuentaAtrasDuracion = 5;
+    [SerializeField] private string[] nombresObjetos;
+    [SerializeField] private string[] nombresEscenas;
+    private bool cuentaAtras = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                for (int i = 0; i < nombresObjetos.Length; i++)
+                {
+                    if (hit.transform.name == nombresObjetos[i] && !cuentaAtras)
+                    {
+                        StartCoroutine(CuentaAtras(nombresEscenas[i]));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator CuentaAtras(string escenaDestino)
+    {
+        cuentaAtras = true;
+
+        for (int i = cuentaAtrasDuracion; i >= 0; i--)
+        {
+            cuentaAtrasTexto.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        SceneManager.LoadScene(escenaDestino);
+        cuentaAtras = false;
     }
 }
